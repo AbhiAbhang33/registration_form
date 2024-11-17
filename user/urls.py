1975d3b7856a8e8ede37af6fe12af224cb4cc647
Abhi <abhangabhaishek99200@gmail.com>
2024-11-17 10:45:26 +0530

    Final Commit

---
+++ user/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import register, user_login, home
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('register/', register, name='register'),
+    path('login/', user_login, name='login'),
+]
+
---
+++ user/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+from django.contrib.auth import login, authenticate
+
+def home(request):
+    return render(request, 'home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')  # Redirect to home page after successful registration
+    else:
+        form = RegistrationForm()
+    return render(request, 'register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # Redirect to home page after successful login
+    return render(request, 'login.html')
+
